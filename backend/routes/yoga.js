39daf0dfd0a5dee77adc29d62b52ab3bc85c2a06
Navvv13 <navneetassi01331@gmail.com>
2022-12-12 16:31:39 +0530

    error fixed

--- backend/routes/yoga.js
+++ backend/routes/yoga.js
@@ -6,39 +6,39 @@ const yogaRouter = Router();
 yogaRouter.post('/insert', async(req,res) => {   
     try {
         if(req.body.name == ""){
-            res.status(400).send({
+            return res.status(400).send({
                 "success":false,
                 "message":"Please Input Name"
             });
         }
         if(req.body.email == ""){
-            res.status(400).send({
+            return res.status(400).send({
                 "success":false,
                 "message":"Please Input Email"
             });
         }
         if(req.body.phoneNumber == ""){
-            res.status(400).send({
+            return res.status(400).send({
                 "success":false,
                 "message":"Please Input PhoneNumber"
             });
         }
         if(!req.body.dob){
-            res.status(400).send({
+            return res.status(400).send({
                 "success":false,
                 "message":"Please Input DOB"
             });
         }
 
         if(!req.body.gender){
-            res.status(400).send({
+            return res.status(400).send({
                 "success":false,
                 "message":"Please Input gender"
             });
         }
 
         if(!req.body.slot){
-            res.status(400).send({
+            return res.status(400).send({
                 "success":false,
                 "message":"Please Input slot"
             });
@@ -54,14 +54,14 @@ yogaRouter.post('/insert', async(req,res) => {
         }
 
         if(temp.age < 18) {
-            res.status(400).send({
+            return res.status(400).send({
                 "success":false,
                 "message":"Age should be greater than 18"
             });
         }
 
         if(temp.age > 65) {
-            res.status(400).send({
+            return res.status(400).send({
                 "success":false,
                 "message":"Age should be less than 65"
             });
@@ -71,32 +71,32 @@ yogaRouter.post('/insert', async(req,res) => {
             if(successPayment()){
                 try{
                     const savedEntry = await newEntry.save();
-                    res.status(201).send({
+                    return res.status(201).send({
                         "success":true,
                         "message":"Registration Successful!",
                         "data":savedEntry
                     })
                 }catch(err){
-                    res.status(400).send({
+                    return res.status(400).send({
                         "success":false,
                         "message":err.msg
                     });
                 }
             }else{
-                res.status(400).send({
+                return res.status(400).send({
                     "success":false,
                     "message":"Payment Failed!"
                 });
             }
 
         }catch(err){
-            res.status(400).send({
+            return res.status(400).send({
                 "success":false,
                 "message":err.msg
             });
         }
     }catch(err){
-        res.status(400).send({
+        return res.status(400).send({
             "success":false,
             "message":err.msg
         });
